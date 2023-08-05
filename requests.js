e5a8430c5da697bc38ee2f85979cb96de3ea198c
Ranjesh kumar roy <21cs3039@rgipt.ac.in>
2023-08-05 15:38:31 +0530

    Add files via upload

---
+++ requests.js
@@ -0,0 +1,51 @@
+// Initialize Firebase (ADD YOUR OWN DATA)
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('fname');
+  var grain = getInputVal('grain');
+  var  = getInputVal('veg');
+  var phone = getInputVal('pulses');
+  var message = getInputVal('note');
+
+  // Save message
+  saveMessage(name, company, email, phone, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    company:company,
+    email:email,
+    phone:phone,
+    message:message
+  });
+}
